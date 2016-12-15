5cac03988dcaa12346f16bc4f317f1b32110540f
anhquoctran <quoc.trananh@students.hueic.edu.vn>
2016-12-16 00:47:38 +0700

    Commit from source tree Implemented devices models

--- models/devices.js
+++ models/devices.js
@@ -1,10 +1,18 @@
 function Device() {
-    this.IdDevice;
-    this.IdEndPoint;
-    this.Name;
-    this.Description;
-    this.Unit;
-    this.MinThreshold;
-    this.MaxThreshol;
+
+    var db = require("../database");
+
+    this.getDevice = function(idDevice) {
+        db.acquire((err, conn) => {
+            conn.query("SELECT * FROM devicetbl WHERE id=?", idDevice, (err, result) => {
+                if (err)
+                    throw err;
+
+            });
+        });
+    };
+    this.getAllDevice = function() {
+
+    }
 };
 module.exports = new Device();
