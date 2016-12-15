c282aaab2a6dbb9c91cc86fb498be79eb121dba1
anhquoctran <quoc.trananh@students.hueic.edu.vn>
2016-12-15 13:56:11 +0700

    add module

--- config.js
+++ config.js
@@ -1,9 +1,19 @@
 var express = require("express");
-
+var bodyparser = require("body-parser");
 module.exports = function(app) {
     app.set("view engine", "html");
     app.engine('html', require('ejs').renderFile);
     app.set("views", __dirname + "/views");
 
     app.use('/public', express.static(__dirname + '/public'));
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({ extended: true }));
+
+    var router = express.Router();
+
+    router.get("/", (req, res) => {
+        res.json({ message: "Welcome to Environment Monitoring API", version: "1.0" });
+    });
+
+    app.use("/api/v1/", router);
 };
