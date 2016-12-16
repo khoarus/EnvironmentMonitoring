28ee71229592076f2a615a69985925975d282845
anhquoctran <quoc.trananh@students.hueic.edu.vn>
2016-12-16 23:16:55 +0700

    implemented models

--- models/users.js
+++ models/users.js
@@ -2,40 +2,59 @@ var crypto = require("crypto");
 var db = require("../controllers/database");
 
 function User() {
-    this.register = function(username, password, firstname, lastname, callback) {
+    this.register = (username, password, firstname, lastname, callback) => {
         var passwordhash = encrypt(password);
         db.connection.query("INSERT INTO userstbl(FirstName, LastName, Username, Password) VALUE(?, ?, ?, ?)", [firstname, lastname, username, passwordhash], (err, result) => {
             callback(result.affectedRows > 0);
         });
     };
 
-    this.login = function(username, password, callback) {
+    this.login = (username, password, callback) => {
         var passwordhash = encrypt(password);
         db.connection.query("SELECT * FROM userstbl WHERE Username = ? AND Password = ?", [username, passwordhash], (err, result) => {
             if (err) throw err;
             if (result.length > 0) {
-                callback(result);
+                callback(result, true);
             } else {
-                callback(null);
+                callback(null, false);
             }
         });
     };
 
     function encrypt(data) {
         return crypto.createHash('md5').update(data).digest("hex");
-    }
+    };
 
-    this.getUserById = function(userId, callback) {
-        db.connection.query("SELECT * FROM userstbl WHERE IdUser = ?", userId, (err, result) => {
+    this.getUserById = (userId, callback) => {
+        db.connection.query("SELECT IdUser ID, Firstname FirstName, Lastname LastName, Username, Password FROM userstbl WHERE IdUser = ?", userId, (err, result) => {
             if (!err) {
                 callback(result);
             } else {
                 throw err;
             }
         });
-    }
+    };
 
-    this.changePassword = function(username, password, callback) {
+    this.getUsers = (callback) => {
+        db.connection.query("SELECT IdUser ID, Firstname FirstName, Lastname LastName, Username, Password FROM userstbl", (err, result) => {
+            if (err) {
+                throw err;
+            }
+            if (result.lenth > 0) {
+                callback(result);
+            } else callback(null);
+        });
+    };
+
+    this.updateUserInformation = (userId, firstname, lastname, username, password, callback) => {
+        db.connection.query("UPDATE userstbl User SET FirstName = ?, LastName = ?, Username = ?, Password = ? WHERE IdUser = ?", [firstname, lastname, username, password, userId], (err, result) => {
+            if (err) throw
+            if (result.affectedRows > 0) callback(result);
+            else callback(null);
+        });
+    };
+
+    this.changePassword = (username, password, callback) => {
         var passwordhash = encrypt(password);
         db.connection.query("UPDATE userstbl SET Password = ? WHERE Username = ?", [passwordhash, username], (err, result) => {
             if (err) throw err;
@@ -44,7 +63,17 @@ function User() {
             } else callback(false);
         });
     };
-};
 
+    this.deleteUser = (userId, callback) => {
+        db.connection.query("DELETE FROM userstbl WHERE IdUser = ?", [userId], (err, result) => {
+            if (err) throw err;
+            if (result.affectedRows > 0) {
+                callback(true);
+            } else {
+                callback(false);
+            }
+        });
+    };
+};
 
 module.exports = new User();
--- models/values.js
+++ models/values.js
@@ -1,6 +1,5 @@
 function Values() {
     var db = require("../controllers/database");
-
     this.putValue = (value, time, idDevice, callback) => {
         db.connection.query("INSERT valuetbl(id_device, time, value) VALUE(?, ?, ?)", [idDevice, time, value], (err, result) => {
             if (err) throw err;
@@ -16,14 +15,19 @@ function Values() {
         db.connection.query("SELECT V.id IdValue, V.id_device IdDevice, V.time Time, V.value Value FROM valuetbl V LEFT JOIN devicetbl D ON D.id = IdDevice WHERE IdDevice = ?", idDevice, (err, result) => {
             if (err) throw err;
             if (result.length > 0) {
-                callback(true);
-            } else callback(false);
+                callback(result);
+            } else callback(null);
         });
     };
 
-    this.getLatestValue = (idDevice, idValue, callback) => {
-        db.connection.query("SELECt ", [], (err, result) => {
-
+    this.getLatestValue = (idDevice, callback) => {
+        db.connection.query("SELECT * FROM valuetbl ORDER BY id DESC LIMIT 1", [], (err, result) => {
+            if (err) throw err;
+            if (result.length > 0) {
+                callback(result);
+            } else {
+                callback(null);
+            }
         });
     };
 
