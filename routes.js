a0372232770966e0a099801a8721d87c8e4fa615
anhquoctran <quoc.trananh@students.hueic.edu.vn>
2016-12-16 15:24:50 +0700

    fix api request

--- routes.js
+++ routes.js
@@ -18,16 +18,4 @@ module.exports = function(app) {
     app.post('/login', (req, res) => {
 
     });
-    app.post('/register', (req, res) => {
-        var username = req.body.username;
-        var password = req.body.password;
-        var firstname = req.body.firstname;
-        var lastname = req.body.lastname;
-        var result = users.register(username, password, firstname, lastname);
-        if (result) {
-            res.send("OK");
-        } else {
-            res.send("ERROR");
-        }
-    });
 };
