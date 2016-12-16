50a89390e49807b9e163c156bd8472919a1dc89c
anhquoctran <quoc.trananh@students.hueic.edu.vn>
2016-12-16 22:08:34 +0700

    Fix API features and model functions, edit Readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# EnvironmentMonitoring
-Project monitoring environmental parameters
+# Environment Monitoring API Documentation
+This is documentation using for Environment Monitoring.
+
+This API including 4 parts:
+```
+* Users
+* Devices
+* Endpoints
+* Values
+```
+
+## Users
+
+#### Login
+User must login to access all services provided by this API. An users account have ```username```, ```password```, ```lastname``` and ```firstname```.
+To login, we will use ``HTTP POST`` method to send request ``username`` and ``password`` to API
+``POST /users/login``
+We need add 2 parameters to body: ``username`` and ``password``.
--- models/values.js
+++ models/values.js
@@ -1,18 +1,31 @@
 function Values() {
     var db = require("../database");
 
-    this.putValue = (value, time, idDevice) => {
-        var res = false;
-        db.connection.query("insert valuetbl(id_device, time, value) values(?, ?, ?)", [idDevice, time, value], (err, conn) => {
-
+    this.putValue = (value, time, idDevice, callback) => {
+        db.connection.query("INSERT valuetbl(id_device, time, value) VALUE(?, ?, ?)", [idDevice, time, value], (err, result) => {
+            if (err) throw err;
+            if (result.affectedRows > 0) {
+                callback(true);
+            } else {
+                callback(false);
+            }
         });
     }
 
-    this.getValue = (idDevice) => {
+    this.getValue = (idDevice, callback) => {
+        db.connection.query("SELECT V.id IdValue, V.id_device IdDevice, V.time Time, V.value Value FROM valuetbl V LEFT JOIN devicetbl D ON D.id = IdDevice WHERE IdDevice = ?", idDevice, (err, result) => {
+            if (err) throw err;
+            if (result.length > 0) {
+                callback(true);
+            } else callback(false);
+        });
+    }
+
+    this.getLatestValue = (idDevice, idValue, callback) => {
 
     }
 
-    this.changeValue = (idValue, value) => {
+    this.changeValue = (idValue, value, callback) => {
 
     }
 };
