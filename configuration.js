e7abaf5f07ff0d6068665565d7eb563766664ac4
anhquoctran <quoc.trananh@students.hueic.edu.vn>
2016-12-17 13:22:43 +0700

    fix absolute path

--- configuration.js
+++ configuration.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var bodyparser = require("body-parser");
 var session = require('express-session');
+var path = require('path');
 module.exports = function(app) {
     app.set("view engine", "html");
     app.engine('html', require('ejs').renderFile);
-    app.set("views", __dirname + "../views");
-    app.use('../public', express.static(__dirname + '/public'));
+    app.set('views', __dirname + '/views');
+    app.use('/public', express.static(__dirname + '/public'));
     app.use(bodyparser.json());
     app.use(bodyparser.urlencoded({ extended: true }));
     app.use(session({
@@ -14,5 +15,5 @@ module.exports = function(app) {
         saveUninitialized: true
     }));
     var router = express.Router();
-    require('./api')(app, router);
+    require('./controllers/api')(app, router);
 };
