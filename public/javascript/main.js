dfb6aac43dc3b5484fb0cf32318549956ecb071d
hoangbaovu <baovuhbv@gmail.com>
2016-12-19 01:17:04 +0700

    fix responsive sidebar

---
+++ public/javascript/main.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+		$(window).on('resize', function () {
+		  if ($(window).width() > 768) $('#sidebar-collapse').collapse('show')
+		})
+		$(window).on('resize', function () {
+		  if ($(window).width() <= 767) $('#sidebar-collapse').collapse('hide')
+		})
+});
