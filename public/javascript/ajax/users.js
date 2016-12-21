e7653ba98747d1bf1d7e165561470d2b6cf623f9
khoarus <ndkhoa82@gmail.com>
2016-12-21 16:04:43 +0700

    Discard all changes

---
+++ public/javascript/ajax/users.js
@@ -0,0 +1,40 @@
+$(function() {
+
+    var API = "http://203.113.167.3:8082/core/api/v1/users";
+
+    $.getJSON(API, function(data) {
+
+        $.each(data.Result, function() {
+            $('.table tbody').append('\
+        <tr>\
+          <td>' + this.ID + '</td>\
+          <td>' + this.LastName + '</td>\
+          <td>' + this.FirstName + '</td>\
+          <td>' + this.Username + '</td>\
+          <td>' + this.Role + '</td>\
+          <td>\
+            <a href="/account/detail" class="table-link">\
+              <span class="fa-stack">\
+                <i class="fa fa-square fa-stack-2x"></i>\
+                <i class="fa fa-search-plus fa-stack-1x fa-inverse"></i>\
+              </span>\
+            </a>\
+            <a href="/account/edit" class="table-link">\
+              <span class="fa-stack">\
+                <i class="fa fa-square fa-stack-2x"></i>\
+                <i class="fa fa-pencil fa-stack-1x fa-inverse"></i>\
+              </span>\
+            </a>\
+            <a href="#" class="table-link">\
+              <span class="fa-stack">\
+                <i class="fa fa-square fa-stack-2x"></i>\
+                <i class="fa fa-trash-o fa-stack-1x fa-inverse"></i>\
+              </span>\
+            </a>\
+          </td>\
+        </tr>\
+      ');
+        });
+
+    });
+});
