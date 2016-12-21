8ee2b5ce68e8c2ee00d22adcce11172295f9a222
hoangbaovu <baovuhbv@gmail.com>
2016-12-21 10:58:46 +0700

    edit chartjs

--- public/javascript/Chart-data.js
+++ public/javascript/Chart-data.js
@@ -41,15 +41,34 @@ Chart.helpers.extend(Chart.controllers.line.prototype, {
 var myChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
     datasets: [{
-      label: 'Device 1',
-      data: [50, 90, 83, 45, 32, 13],
+            label: "My First dataset",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 95, 56, 55, 40, 67, 45, 12, 44, 2],
+            spanGaps: false,
     }],
     // This, if it exists at all, defines the highlight region.
     yHighlightRange : {
       begin: 80,
-      end: 10
+      end: 10,
     }
   },
   options: {
