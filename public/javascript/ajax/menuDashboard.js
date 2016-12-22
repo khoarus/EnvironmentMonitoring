a765dec16c51518454774fa6c85b8e34da7d94f5
hoangbaovu <baovuhbv@gmail.com>
2016-12-22 10:39:06 +0700

    get json endpoints

--- public/javascript/ajax/menuDashboard.js
+++ public/javascript/ajax/menuDashboard.js
@@ -1,16 +1,32 @@
-$(function() {
+$(getEndpoint(0));
 
-    var API = "/core/api/v1/endpoints/fetch";
+function getEndpoint(id) {
+    var API = "";
+    if (id==0) 
+    {
+         API = "/core/api/v1/endpoints/fetch";
+         $.getJSON(API, function(data) {
 
-    $.getJSON(API, function(data) {
-
-        $.each(data.Result, function() {
-            $('.nav-tabs').append('\
-              <li>\
-                <a href="/endpoints/fetch/' + this.ID + '">' + this.name + '</a>\
-              </li>\
+            $.each(data.Result, function() {
+                $('.nav-tabs').append('\
+                <li>\
+                    <a  onclick="getEndpoint('+ this.ID +')" >' + this.name + '</a>\
+                </li>\
             ');
-        });
+            });
 
-    });
-});
+        });
+    } 
+    else 
+    {
+        API = "/core/api/v1/endpoints/fetch/"+id;
+
+        $.getJSON(API, function(data) {
+            // console.log(data);
+
+            
+        });
+    }
+}
+
+
