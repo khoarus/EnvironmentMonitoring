7f9a2b16a0bb609569bc666aa82e7b0459b3b336
anhquoctran <quoc.trananh@students.hueic.edu.vn>
2016-12-22 15:39:48 +0700

    add session for route

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ var users = require('./models/users');
 module.exports = function(app) {
     app.get('/', (req, res) => {
         if (req.session && req.session.result) {
-            users.getUserById(req.session.ID, (result) => {
+            users.getUserById(req.session.result.ID, (result) => {
                 if (result) {
                     res.render('index', {
                         title: 'Dashboard',
@@ -20,7 +20,7 @@ module.exports = function(app) {
 
     app.get('/login', (req, res) => {
         if (req.session && req.session.result) {
-            users.getUserById(req.session.ID, (result) => {
+            users.getUserById(req.session.result.ID, (result) => {
                 if (result) {
                     res.render('index', { title: 'Dashboard' });
                 } else {
@@ -28,11 +28,12 @@ module.exports = function(app) {
                 }
             });
         } else {
-            res.render("login", { title: 'Đăng nhập' });
+            res.redirect("login", { title: 'Đăng nhập' });
         }
     });
 
     app.post('/login', (req, res) => {
+
         var username = req.body.username,
             password = req.body.password;
         if (!username || !password || (!username && !password)) {
@@ -43,7 +44,7 @@ module.exports = function(app) {
                     req.session.user = result;
                     res.redirect('/index');
                 } else {
-                    res.render("login", { error: "Sai tên đăng nhập hoặc mật khẩu" });
+                    res.redirect("/login", { error: "Sai tên đăng nhập hoặc mật khẩu" });
                 }
             });
         }
@@ -51,7 +52,7 @@ module.exports = function(app) {
 
     app.get('/register', (req, res) => {
         if (req.session && req.session.user) {
-            users.getUserById(req.session.ID, (result) => {
+            users.getUserById(req.session.result.ID, (result) => {
                 if (result) {
                     res.redirect("/index");
                 } else {
@@ -69,27 +70,38 @@ module.exports = function(app) {
         var password = req.body.password;
         var firstname = req.body.firstname;
         var lastname = req.body.lastnname;
-        if (!username || !password || !firstname || !lastname) {
-            res.render('register', { title: "Đăng ký" });
-        } else {
-            users.register(username, password, firstname, lastname, (result) => {
-                if (result && result === true) {
-                    res.redirect('/login');
+
+        if (req.session && req.session.result) {
+            users.getUserById(req.session.result.ID, (result) => {
+                if (result) {
+                    if (!username || !password || !firstname || !lastname) {
+                        res.render('register', { title: "Đăng ký" });
+                    } else {
+                        users.register(username, password, firstname, lastname, (result) => {
+                            if (result && result === true) {
+                                res.redirect('/login');
+                            } else {
+                                res.render('register', { error: "Có lỗi xảy ra. Không thể đăng ký tài khoản!" });
+                            }
+                        });
+                    }
                 } else {
-                    res.render('register', { error: "Có lỗi xảy ra. Không thể đăng ký tài khoản!" });
+                    res.redirect('/index');
                 }
             });
+        } else {
+            res.redirect('/redirect');
         }
     });
 
     app.get('/logout', (req, res) => {
         req.session.reset();
-        res.render('login');
+        res.redirect('/login');
     });
+
     app.get('/create', (req, res) => {
         res.render("create");
     });
-
 
     // User
     app.get('/account', (req, res) => {
@@ -99,23 +111,45 @@ module.exports = function(app) {
     });
 
     app.get('/account/create', (req, res) => {
-        res.render('accountCreate', {
-            title: 'Tạo tài khoản',
-        });
+        if (req.session && req.session.result) {
+            users.getUserById(req.session.result.ID, (result) => {
+                if (result) {
+                    res.render('accountCreate', {
+                        title: 'Tạo tài khoản',
+                    });
+                } else {
+                    res.redirect('/login');
+                }
+            });
+        } else {
+            res.redirect('/login');
+        }
+
     });
 
     app.get('/account/detail/:id', (req, res) => {
-        //console.log(req.params.id);
-
-        if (!req.params.id) {
-            res.redirect('/account');
+        if (req.session && req.session.result) {
+            var id = req.params.id;
+            users.getUserById(req.session.result.ID, (result) => {
+                if (result) {
+                    if (!id) {
+                        res.redirect('/account');
+                    } else {
+                        res.render('accountDetail', {
+                            title: 'Thông tin chi tiết',
+                        });
+                    }
+                } else {
+                    res.redirect('/login');
+                }
+            });
         }
-        res.render('accountDetail', {
-            title: 'Thông tin chi tiết',
-        });
     });
 
     app.get('/account/edit', (req, res) => {
+        if (req.session && req.session.result) {
+
+        }
         res.render('accountEdit', {
             title: 'Chỉnh sửa tài khoản',
         });
@@ -172,4 +206,5 @@ module.exports = function(app) {
             title: 'Chỉnh sửa thiết bị',
         });
     });
+
 };
