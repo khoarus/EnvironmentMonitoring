69f82b98daaca6a37344f1badc71a0bfe192fa50
anhquoctran <quoc.trananh@students.hueic.edu.vn>
2016-12-23 21:05:45 +0700

    fix pug render engine

--- configuration.js
+++ configuration.js
@@ -3,15 +3,17 @@ var bodyparser = require("body-parser");
 var cookieParser = require('cookie-parser')
 var session = require('express-session');
 var fs = require('fs');
+var pug = require('pug');
 module.exports = function(app) {
     app.set("view engine", "pug");
-    app.engine('pug', (filepath, options, callback) => {
+    app.engine('pug', pug.renderFile, (filepath, options, callback) => {
         fs.readFile(filepath, (err, content) => {
             if (err) return callback(err);
             var rendered = content.toString().replace("#{title}", options.message).replace("#{error}", options.message);
             return callback(null, rendered);
         });
     });
+
     app.set('views', __dirname + '/views');
     app.use('/public', express.static(__dirname + '/public'));
     app.use(bodyparser.json());
