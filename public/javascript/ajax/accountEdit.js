42b0870bdef384095f212c3cd4a1be94c13ae516
hoangbaovu <baovuhbv@gmail.com>
2016-12-25 03:27:36 +0700

    add "name" accountEdit.pug

--- public/javascript/ajax/accountEdit.js
+++ public/javascript/ajax/accountEdit.js
@@ -6,4 +6,25 @@ $(function() {
     var API = "/core/api/v1/users" + id;
 
     alert(id);
+
+    function editForm(){
+
+        var users = {
+            lastName : $("lastName").val,
+            firstName : $("firstName").val,
+            username : $("username").val,
+            password : $("password").val,
+            passwordRepeat : $("password-repeat").val,
+        }
+
+
+        $.ajax({
+            type: "POST",
+            url:  "update",
+            data: users,
+            success: function(result) {
+                $('')
+            }
+        });
+    }
 });
